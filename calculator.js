63f10d5bd0c29e318c4e1ad864e3c83d856c887d
AdhityaRdt <rahmanadhitya2201@gmail.com>
2018-07-24 11:26:17 +0700

    ready for CI

---
+++ calculator.js
@@ -0,0 +1,54 @@
+'use strict'
+
+class Calculator {
+  //write your code here
+  constructor (inputNumber) {
+    this.number = inputNumber
+  }
+  add (addNumber) {
+    this.number = this.number + addNumber
+    return this
+  }
+  substract (substractNumber) {
+    this.number = this.number - substractNumber
+    return this
+  }
+  multiply (multiplyNumber) {
+    this.number = this.number * multiplyNumber
+    return this
+  }
+  divide (divideNumber) {
+    this.number = this.number / divideNumber
+    return this
+  }
+  square (powNumber) {
+    this.number = Math.pow(this.number,powNumber)
+    return this
+  }
+  squareRoot () {
+    this.number = Math.sqrt(this.number)
+    return this
+  }
+  circleArea(){// menghitung luas dimana this.number sebagai radius
+    this.number = 3.14 * Math.pow(this.number,2)
+    return this
+  }
+  circleRound(){// mengitung keliling dimana this.number sebagai radius
+    this.number = 2 * 3.14 * this.number
+    return this
+  }
+}
+
+module.exports = Calculator
+
+/** note : you can use several features from ecmascript, such as:
+* - Classes
+* - Default Parameters
+* - Destructured Assignment
+* - Template Literals
+* - Method Chaining
+*/
+
+module.exports = Calculator
+let calculator = new Calculator(10)
+console.log(calculator.add(10).substract(2).square(2).squareRoot().circleRound().number);
---
+++ calculator.test.js
@@ -0,0 +1,51 @@
+
+const expect = require ('chai').expect
+const Calculator = require ('./calculator.js')
+
+describe('calculator.js with initial value 100',()=>{
+  it('should return 105',()=>{
+    const calcu = new Calculator(100)
+    let result = calcu.add(5).number
+    expect(result).to.equal(105)
+  })
+  it('should return 95',()=>{
+    const calcu = new Calculator(100)
+    let result = calcu.substract(5).number
+    expect(result).to.equal(95)
+  })
+  it('should return 500',()=>{
+    const calcu = new Calculator(100)
+    let result = calcu.multiply(5).number
+    expect(result).to.equal(500)
+  })
+  it('should return 20',()=>{
+    const calcu = new Calculator(100)
+    let result = calcu.divide(5).number
+    expect(result).to.equal(20)
+  })
+  it('should return 100',()=>{
+    const calcu = new Calculator(10)
+    let result = calcu.square(2).number
+    expect(result).to.equal(100)
+  })
+  it('should return 10',()=>{
+    const calcu = new Calculator(100)
+    let result = calcu.squareRoot().number
+    expect(result).to.equal(10)
+  })
+  it('should return 314',()=>{
+    const calcu = new Calculator(10)
+    let result = calcu.circleArea().number
+    expect(result).to.equal(314)
+  })
+  it('should return 62',()=>{
+    const calcu = new Calculator(10)
+    let result = Math.floor(calcu.circleRound().number)
+    expect(result).to.equal(62)
+  })
+  it('should return 113.04',()=>{
+    const calcu = new Calculator(10)
+    let result = calcu.add(10).substract(2).square(2).squareRoot().circleRound().number
+    expect(result).to.equal(113.04)
+  })
+})
